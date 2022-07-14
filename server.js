704c9ea617efdca3af4cce91451dd9dabf99e42e
Marcel Qayoom-Taylor <marcelqayoomtaylor@gmail.com>
2022-07-15 09:30:46 +1000

    Working api

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const app = express()
+const PORT = 8000
+
+const rappers = {
+    '21 savage':{
+        'age': 29,
+        'birthname': 'Sheyaa Bin Abraham-Joseph',
+        'birthLocation': 'London, England'
+    },
+    'chance the rapper':{
+        'age': 29,
+        'birthname': 'Chancellor Bennet',
+        'birthLocation': 'Chicago, Illinois'
+    }, 
+    'dylan':{
+        'age': 29,
+        'birthname': 'Dylan',
+        'birthLocation': 'Dylan'
+    }, 
+}
+
+app.get('/', (request, response) => {
+    response.sendFile(__dirname + '/index.html') // __dirname essentially is whereever this file is located, start looking from there
+})
+
+app.get('/api/:rapperName', (request, response) => {
+    const rappersName = request.params.rapperName.toLowerCase() 
+    //response.json(rappers) // sends json
+    if (rappers[rappersName]){
+        response.json(rappers[rappersName])
+    } else {
+        response.json(rappers['dylan'])
+    }
+})
+
+app.listen(process.env.PORT || PORT, ()=>{
+    console.log(`The server is running on port ${PORT}! You better go catch it!`)
+})
